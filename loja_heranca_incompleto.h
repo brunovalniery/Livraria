5f1cfb6844687464cf25ba1251e9dc036469ee0c
Bruno <brunovalniery@gmail.com>
2019-09-10 17:00:12 -0300

    Adding important commands

--- loja_heranca_incompleto.h
+++ loja_heranca_incompleto.h
@@ -9,16 +9,16 @@ using namespace std;
 class Produto
 {
 private:
-  string nome;
-  unsigned preco;
+    string nome;
+    unsigned preco;
 public:
-  inline Produto(const string& N="", unsigned P=0): nome(N), preco(P) {}
+    inline Produto(const string& N="", unsigned P=0): nome(N), preco(P) {}
 
-  bool ler(istream &I);
-  void salvar(ostream &O) const;
-  void digitar();
-  // Define a funcao imprimir como sendo salvar em cout
-  inline void imprimir() const {salvar(cout);}
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void digitar();
+    // Define a funcao imprimir como sendo salvar em cout
+    inline void imprimir() const {salvar(cout);}
 };
 
 inline istream& operator>>(istream &I, Produto &P) {P.digitar(); return I;} //utiliza método público, por isso não é necessário a palavra reservada friend.
@@ -31,45 +31,45 @@ class Livro : public Produto{
 private:
     string autor;
 public:
-    bool ler(istream &I){Produto::ler()};
-    void salvar(ostream &O) const Produto::salvar();
-    void digitar() Produto::digitar();
-    inline void imprimir() const {salvar(cout); Produto::imprimir()}
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void digitar();
+    inline void imprimir() const salvar(cout);
 };
 
 class CD : public Produto{
 private:
-    int numfaixas;
+    unsigned numfaixas;
 public:
-    bool ler(istream &I) Produto::ler();
-    void salvar(ostream &O) const Produto::salvar();
-    void digitar() Produto::digitar();
-    inline void imprimir() const {salvar(cout); Produto::imprimir()}
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void digitar();
+    inline void imprimir() const salvar(cout);
 };
 
 class DVD : public Produto{
 private:
-    int duracao;
+    unsigned duracao;
 public:
-    bool ler(istream &I) Produto::ler();
-    void salvar(ostream &O) const Produto::salvar();
-    void digitar() Produto::digitar();
-    inline void imprimir() const {salvar(cout); Produto::imprimir()}
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void digitar();
+    inline void imprimir() const salvar(cout);
 };
 
 class ListaLivro //classe que envolve alocação dinâmica de memória
 {
 private:
-  Livro *x; //array dinâmico de livros
-  unsigned N; //guardar a dimensão do array
+    Livro *x; //array dinâmico de livros
+    unsigned N; //guardar a dimensão do array
 public:
   // Inclua os construtores, destrutor e operadores necessarios
-  void incluir(const Livro &L);
-  bool excluir(unsigned id);
+    void incluir(const Livro &L);
+    bool excluir(unsigned id);
 
-  bool ler(istream &I);
-  void salvar(ostream &O) const;
-  void imprimir() const;
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void imprimir() const;
 };
 
 // =========================================
@@ -77,14 +77,28 @@ public:
 // =========================================
 class ListaCD{
 private:
+    CD *x; //array dinâmico de livros
+    unsigned N; //guardar a dimensão do array
 public:
+    void incluir(const CD &L);
+    bool excluir(unsigned id);
 
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void imprimir() const;
 };
 
 class ListaDVD{
 private:
+    DVD *x; //array dinâmico de livros
+    unsigned N; //guardar a dimensão do array
 public:
+    void incluir(const DVD &L);
+    bool excluir(unsigned id);
 
+    bool ler(istream &I);
+    void salvar(ostream &O) const;
+    void imprimir() const;
 };
 
 class Loja
